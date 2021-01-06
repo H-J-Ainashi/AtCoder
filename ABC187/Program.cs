cff10d93f0bf19901bfc1ec53fbc3f7b006a723d
Electronical-ARAI-san <61216147+H-J-Ainashi@users.noreply.github.com>
2021-01-06 09:33:22 +0900

    ABC187-A(Complete)

---
+++ ABC187/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtCoder
+{
+    class Program
+    {
+        enum Questions
+        {
+            A, B, C, D, E, F
+        }
+
+        static void Main()
+        {
+
+            const Questions question = Questions.B;
+
+            switch (question)
+            {
+                case Questions.A:
+                    {
+                        var args = Console.ReadLine().Split(' ').ToArray();
+                        var s = args.Select(x => x.Select(y => int.Parse(y.ToString())).Sum());
+                        Console.WriteLine(s.Max());
+                    }
+                    break;
+                case Questions.B:
+
+                    break;
+                case Questions.C:
+                    break;
+                case Questions.D:
+                    break;
+                case Questions.E:
+                    break;
+                case Questions.F:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
