babd4c89a8a863481afc71d259679465457bbd25
Electronical-ARAI-san <61216147+H-J-Ainashi@users.noreply.github.com>
2021-01-17 20:05:17 +0900

    ARC111-A(Complete)

---
+++ ARC111/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtCoder
+{
+    class Program
+    {
+        enum Questions
+        {
+            A, B, C, D, E, F
+        }
+
+        static void Main()
+        {
+
+            const Questions question = Questions.A;
+
+            switch (question)
+            {
+                case Questions.A:
+                    {
+                        var args = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToArray();
+                        var n = args[0];
+                        var m = args[1];
+                        var m2 = m * m;
+                        var ans = 1L;
+
+                        for (long mod_m2 = 10; n != 0; n >>= 1)
+                        {
+                            if ((n & 1) != 0)
+                            {
+                                ans *= mod_m2;
+                                ans %= m2;
+                            }
+                            mod_m2 *= mod_m2;
+                            mod_m2 %= m2;
+                        }
+
+                        Console.WriteLine((ans / m) % m);
+
+                    }
+                    break;
+                case Questions.B:
+                    break;
+                case Questions.C:
+                    break;
+                case Questions.D:
+                    break;
+                case Questions.E:
+                    break;
+                case Questions.F:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
